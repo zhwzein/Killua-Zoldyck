7cda26831be534fd3c4f4b9e8dc40dcfeeed1f19
zahwazein <zhwzein.me@gmail.com>
2022-04-20 02:55:10 +0700

    Update cekapi.js

--- commands/stalker/cekapi.js
+++ commands/stalker/cekapi.js
@@ -5,19 +5,33 @@ module.exports = {
     alias: ["apikey"],
     desc: "Apikey Checker From Zenzapis.xyz",
     type: "stalker",
-    example: `Example : %prefix%command`,
-    exec: async(killua, m, { text, command, prefix, toUpper }) => {
+    example: `Example : %prefix%command APIKEY`,
+    exec: async(killua, m, { text }) => {
         global.mess("wait", m)
-        let fetch = await fetchUrl(global.api("zenz", "/api/cekapi", {}, "apikey"))
-        if (fetch.length == 0) return global.mess("error", m)
-        let caption = `Apikey Checker :\n\n`
-        let i = fetch.message
-        caption += `⭔ Created : ${i.created}\n`
-        caption += `⭔ Updated : ${i.updated}\n`
-        caption += `⭔ Username : ${i.username}\n`
-        caption += `⭔ Status : ${i.status}\n`
-        caption += `⭔ ApiKey : ${i.apiKey}\n`
-        caption += `⭔ Todayhit : ${i.today_hit}\n`
-        killua.sendText(m.from, caption, m)
+        try {
+            let fetch = await fetchUrl(global.api("zenz", "/api/cekapi", {apikey: text}))
+            let caption = `Apikey Checker :\n\n`
+            let i = fetch.message
+            caption += `⭔ Id : ${i.id}\n`
+            caption += `⭔ Created : ${i.created}\n`
+            caption += `⭔ Updated : ${i.updated}\n`
+            caption += `⭔ First Name : ${i.firstname}\n`
+            caption += `⭔ Last Name : ${i.lastname}\n`
+            caption += `⭔ Email : ${i.email}\n`
+            caption += `⭔ Username : ${i.username}\n`
+            caption += `⭔ ApiKey : ${i.apiKey}\n`
+            caption += `⭔ Todayhit : ${i.today_hit}\n`
+            caption += `⭔ Totalhit : ${i.total_hit}\n`
+            caption += `⭔ Status : ${i.status}\n`
+            caption += `⭔ Premium : ${i.premium}\n`
+            caption += `⭔ Expired : ${i.premium_expired}\n`
+            caption += `⭔ Active : ${i.active}\n`
+            caption += `⭔ Location : ${i.location}\n`
+            caption += `⭔ Biodata : ${i.biodata}\n`
+            killua.sendFile(m.from, i.profile_image, "", m, { caption })
+        } catch (e) {
+            m.reply('Error / Apikey Not Valid')
+        }
     },
+    isQuery: true
 }

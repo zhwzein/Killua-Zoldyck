c0d254fece62bee4da63be536d2a98674de42896
zahwazein <zhwzein.me@gmail.com>
2022-04-27 03:51:11 +0700

    add listmessage

--- commands/islami/kisahnabi.js
+++ commands/islami/kisahnabi.js
@@ -7,12 +7,49 @@ module.exports = {
     type: "islami",
     example: "Example : %prefix%command <nabi>\n%prefix%command Muhammad",
     exec: async(killua, m, { text }) => {
-        title = text.toLowerCase()
-        global.mess("wait", m)
-        let fetch = await fetchUrl(global.api("zenz", `/islami/kisahnabi/${title}`, {}, "apikey"))
-        if (fetch.result.length == 0) return global.mess("error", m)
-        let teks = `⭔ Nama : ${fetch.result.name}\n⭔ Lahir : ${fetch.result.lahir}\n⭔ Umur : ${fetch.result.age}\n⭔ Lokasi : ${fetch.result.place}\n⭔ Kisah :\n${fetch.result.story}`
-        killua.sendFile(m.from, fetch.result.image, "", m, { caption: teks })
-    },
-    isQuery: true
+        if (text) {
+            title = text.toLowerCase()
+            let fetch = await fetchUrl(global.api("zenz", `/islami/kisahnabi/${title}`, {}, "apikey"))
+            let teks = `⭔ Nama : ${fetch.result.name}\n⭔ Lahir : ${fetch.result.lahir}\n⭔ Umur : ${fetch.result.age}\n⭔ Lokasi : ${fetch.result.place}\n⭔ Kisah :\n${fetch.result.story}`
+            killua.sendFile(m.from, "https://i.pinimg.com/originals/a6/81/c5/a681c55ca1bee611c39d3b4a58712dc3.jpg", "", m, { caption: teks })
+        } else if (!text) {
+            const sections = [{
+                title: "Kisah Nabi",
+                rows: [
+                    {title: "Kisah Nabi Adam", rowId: "kisahnabi adam"},
+                    {title: "Kisah Nabi Idris", rowId: "kisahnabi idris"},
+                    {title: "Kisah Nabi Nuh", rowId: "kisahnabi nuh"},
+                    {title: "Kisah Nabi Hud", rowId: "kisahnabi hud"},
+                    {title: "Kisah Nabi Sholeh", rowId: "kisahnabi sholeh"},
+                    {title: "Kisah Nabi Ibrahim", rowId: "kisahnabi ibrahim"},
+                    {title: "Kisah Nabi Luth", rowId: "kisahnabi luth"},
+                    {title: "Kisah Nabi Ismail", rowId: "kisahnabi ismail"},
+                    {title: "Kisah Nabi Ishaq", rowId: "kisahnabi ishaq"},
+                    {title: "Kisah Nabi Yaqub", rowId: "kisahnabi yaqub"},
+                    {title: "Kisah Nabi Yusuf", rowId: "kisahnabi yusuf"},
+                    {title: "Kisah Nabi Ayyub", rowId: "kisahnabi ayyub"},
+                    {title: "Kisah Nabi Syuaib", rowId: "kisahnabi syuaib"},
+                    {title: "Kisah Nabi Musa", rowId: "kisahnabi musa"},
+                    {title: "Kisah Nabi Harun", rowId: "kisahnabi harun"},
+                    {title: "Kisah Nabi Dzulkifli", rowId: "kisahnabi dzulkifli"},
+                    {title: "Kisah Nabi Daud", rowId: "kisahnabi daud"},
+                    {title: "Kisah Nabi Sulaiman", rowId: "kisahnabi sulaiman"},
+                    {title: "Kisah Nabi Ilyas", rowId: "kisahnabi ilyas"},
+                    {title: "Kisah Nabi Ilyasa", rowId: "kisahnabi ilyasa"},
+                    {title: "Kisah Nabi Yunus", rowId: "kisahnabi yunus"},
+                    {title: "Kisah Nabi Zakariya", rowId: "kisahnabi zakariya"},
+                    {title: "Kisah Nabi Yahya", rowId: "kisahnabi yahya"},
+                    {title: "Kisah Nabi Isa", rowId: "kisahnabi isa"},
+                    {title: "Kisah Nabi Muhammad", rowId: "kisahnabi muhammad"}
+                ]
+            }]
+            const listMessage = {
+                text: "List 25 Nabi",
+                footer: `Powered By https://zenzapis.xyz`,
+                buttonText: "OPEN LIST",
+                sections
+            }
+            const sendMsg = await killua.sendMessage(m.from, listMessage, { quoted: m })
+        }
+    }
 }

9f748214b660eb7bb489051717ab08fe5d38ba4c
zahwazein <zhwzein.me@gmail.com>
2022-04-26 20:23:47 +0700

    Create smeme.js

---
+++ commands/creator/smeme.js
@@ -0,0 +1,36 @@
+const { getRandom } = require("../../lib/Function")
+const request = require('request')
+const fs = require('fs')
+
+module.exports = {
+    name: "smeme",
+    alias: ["mememaker"],
+    desc: "Meme Maker From Memegen",
+    type: "creator",
+    exec: async(killua, m, { command, text, prefix, quoted, mime }) => {
+        if (!quoted) return  m.reply(`Reply to Supported media With Caption ${prefix + command}`)
+        if (/image/.test(mime)) {
+            if (!text.includes('|')) return m.reply(`Example : ${prefix + command} ATAS|BAWAH`)
+            let [a, b] = text.split`|`
+            let download = await killua.downloadAndSaveMediaMessage(quoted)
+            file_name = getRandom('jpg')
+            request({
+                url: global.api("zenz", "/creator/smeme", {text: a, text2: b}, "apikey"),
+                method: 'POST',
+                formData: {
+                    "sampleFile": fs.createReadStream(download)
+                },
+                encoding: "binary"
+            }, async function(error, response, body) {
+                fs.unlinkSync(download)
+                fs.writeFileSync(file_name, body, "binary")
+                ini_buff = fs.readFileSync(file_name)
+                await killua.sendFile(m.from, ini_buff, "", m).then(() => {
+                    fs.unlinkSync(file_name)
+                })
+            });
+        } else {
+            return m.reply(`Reply to Supported media With Caption ${prefix + command}`, m.from, { quoted: m })
+        }
+    }
+}

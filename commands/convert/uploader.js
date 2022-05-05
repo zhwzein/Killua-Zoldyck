72e77cb41631cebca8908facb50ea68c8509f5a1
zahwazein <zhwzein.me@gmail.com>
2022-05-06 05:08:54 +0700

    Create uploader.js
    Result / Gambar Ada di https://zenzapis.xyz/uploader/

---
+++ commands/convert/uploader.js
@@ -0,0 +1,38 @@
+const { getRandom } = require("../../lib/Function")
+let axios = require('axios')
+let BodyForm = require('form-data')
+
+module.exports = {
+    name: "uploader",
+    alias: ["tourl","upload"],
+    desc: "Upload Image To Url",
+    type: "convert",
+    example: "Example : %prefix%command text",
+    exec: async(killua, m, { command, prefix, text, quoted, mime }) => {
+        if (!quoted) return  m.reply(`Reply to Supported media With Caption ${prefix + command}`)
+        if (/image|video|sticker/.test(mime)) {
+            global.mess("wait", m)
+            let download = await killua.downloadMediaMessage(quoted)
+            const form = new BodyForm()
+            form.append('sampleFile', download, { filename: 'fromBot-' + getRandom('jpg') })
+            if (text) {
+                form.append('comment', text)
+            } else if (!text) {
+                form.append('comment', "Killua BOT")
+            }
+            axios.post(global.api("zenz", "/uploader", {}, "apikey"), form.getBuffer(), { headers: { "content-type": `multipart/form-data; boundary=${form._boundary}`}
+            }).then(({ data }) => {
+                let caption = `Convert Image To Url :\n\n`
+                caption += `⭔ Title : ${data.result.originalname}\n`
+                caption += `⭔ Size : ${data.result.size}\n`
+                caption += `⭔ MimeType : ${data.result.mimetype}\n`
+                caption += `⭔ Comment : ${data.result.comment}\n`
+                caption += `⭔ CreatedOn : ${data.result.createdOn}\n`
+                caption += `⭔ Url : https://zenzapis.xyz/uploader/${data.result.originalname}\n`
+                killua.sendFile(m.from, data.result.url, "", m, { caption })
+            })
+        } else {
+            return m.reply(`Reply to Supported media With Caption ${prefix + command}`, m.from, { quoted: m })
+        }
+    }
+}

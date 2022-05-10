6764ecbfedda28d0985abb6e9cc17fda3e5f838d
zahwazein <zhwzein.me@gmail.com>
2022-05-11 00:13:15 +0700

    Update tweetmaker.js

--- commands/creator/tweetmaker.js
+++ commands/creator/tweetmaker.js
@@ -3,7 +3,7 @@ module.exports = {
     alias: ["twc","tweetcomment"],
     desc: "Twitter Comment Maker",
     type: "creator",
-    exec: async(killua, m, { text }) => {
+    exec: async(killua, m, { command, prefix, text }) => {
         if (!text.includes('|')) return m.reply(`Example : ${prefix + command} Text|Username`)
         let [a, b] = text.split`|`
         killua.sendFile(m.from, global.api("zenz", "/creator/maketweet", {text: a, text2: b}, "apikey"), "", m)

174aac2aa5715a329a49552fb6530bb376fde2ac
zahwazein <zhwzein.me@gmail.com>
2022-05-11 00:04:16 +0700

    Update tebakgambar.js

--- commands/entertainment/tebakgambar.js
+++ commands/entertainment/tebakgambar.js
@@ -19,7 +19,7 @@ module.exports = {
         let res = await fetchUrl(global.api("zenz", "/api/tebakgambar", {}, "apikey"))
         let json = await res.result
         game[id] = [
-            await killua.sendFile(m.from, json.img, "", m, { caption: `Answers the following questions\n${json.deskripsi}\n\nTimeout *${(timeout / 1000).toFixed(2)} second(s)*`),
+            await killua.sendFile(m.from, json.img, "", m, { caption: `Answers the following questions\n${json.deskripsi}\n\nTimeout *${(timeout / 1000).toFixed(2)} second(s)*`}),
             json, poin,
             setTimeout(async () => {
                 if (game[id]) await m.reply(`Time has run out!\nthe answer is *${json.jawaban}*`)

5fca7c5fc5bec24f009fb2863392ce0ed5ed22a9
zahwazein <zhwzein.me@gmail.com>
2022-05-14 17:33:50 +0700

    test loli :3

---
+++ commands/randomimage/loli.js
@@ -0,0 +1,70 @@
+const { fetchUrl } = require("../../lib/Function")
+
+module.exports = {
+    name: "loli",
+    alias: ["loli"],
+    desc: "Generate Random loli Image From Apis",
+    type: "randomimage",
+    example: `Example : %prefix%command <type>`,
+    exec: async(killua, m, { text, toUpper }) => {
+        let fetch = await fetchUrl('https://lolikuy.herokuapp.com/')
+        if (text.toLowerCase() === "nsfw") {
+            let buttons = [
+                {buttonId: `loli nsfw`, buttonText: { displayText: 'Loli NSFW'}, type: 1 },
+                {buttonId: `loli sfw`, buttonText: { displayText: 'Loli SFW'}, type: 1 },
+                {buttonId: `loli shota`, buttonText: { displayText: 'Random Shota'}, type: 1 },
+            ]
+            let buttonMessage = {
+                image: { url: fetch.nsfwloli },
+                caption: `Random Image ${toUpper(text)}`,
+                footer: `Powered By https://zenzapis.xyz`,
+                buttons: buttons,
+                headerType: 4
+            }
+            killua.sendMessage(m.from, buttonMessage, { quoted: m })
+        } else if (text.toLowerCase() === "sfw") {
+            let buttons = [
+                {buttonId: `loli nsfw`, buttonText: { displayText: 'Loli NSFW'}, type: 1 },
+                {buttonId: `loli sfw`, buttonText: { displayText: 'Loli SFW'}, type: 1 },
+                {buttonId: `loli shota`, buttonText: { displayText: 'Random Shota'}, type: 1 },
+            ]
+            let buttonMessage = {
+                image: { url: fetch.sfwloli },
+                caption: `Random Image ${toUpper(text)}`,
+                footer: `Powered By https://zenzapis.xyz`,
+                buttons: buttons,
+                headerType: 4
+            }
+            killua.sendMessage(m.from, buttonMessage, { quoted: m })
+        } else if (text.toLowerCase() === "shota") {
+            let buttons = [
+                {buttonId: `loli nsfw`, buttonText: { displayText: 'Loli NSFW'}, type: 1 },
+                {buttonId: `loli sfw`, buttonText: { displayText: 'Loli SFW'}, type: 1 },
+                {buttonId: `loli shota`, buttonText: { displayText: 'Random Shota'}, type: 1 },
+            ]
+            let buttonMessage = {
+                image: { url: fetch.sfwshota },
+                caption: `Random Image ${toUpper(text)}`,
+                footer: `Powered By https://zenzapis.xyz`,
+                buttons: buttons,
+                headerType: 4
+            }
+            killua.sendMessage(m.from, buttonMessage, { quoted: m })
+        }
+        else if (!text) {
+            let buttons = [
+                {buttonId: `loli nsfw`, buttonText: { displayText: 'Loli NSFW'}, type: 1 },
+                {buttonId: `loli sfw`, buttonText: { displayText: 'Loli SFW'}, type: 1 },
+                {buttonId: `loli shota`, buttonText: { displayText: 'Random Shota'}, type: 1 },
+            ]
+            let buttonMessage = {
+                image: { url: fetch.sfwloli },
+                caption: `Random Image Nsfw Loli`,
+                footer: `Powered By https://zenzapis.xyz`,
+                buttons: buttons,
+                headerType: 4
+            }
+            killua.sendMessage(m.from, buttonMessage, { quoted: m })
+        }
+    },
+}

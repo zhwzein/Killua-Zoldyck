7656199568884e6e47262b1c07efca0caebd5021
zahwazein <zhwzein.me@gmail.com>
2022-05-16 02:10:42 +0700

    Update facebook.js

--- commands/downloader/facebook.js
+++ commands/downloader/facebook.js
@@ -9,7 +9,15 @@ module.exports = {
     example: "%prefix%command <url>",
     start: async(killua, m, { text }) => {
         let fetch = await fetchUrl(global.api("zenz", "/downloader/facebook", { url: isUrl(text)[0] }, "apikey"))
-        killua.sendFile(m.from, fetch.result.medias[1].url || fetch.result.medias[0].url, "", m, { caption: `⭔ Title : ${fetch.result.title}\n⭔ Source Url : ${fetch.result.url}` })
+        let caption = `*Facebook Downloader*\n\n`
+        let i = fetch.result
+        caption += `⭔ Title : ${i.title}\n`
+        caption += `⭔ Source Url : ${i.url}\n`
+        caption += `⭔ quality : ${i.medias[0].quality}\n`
+        caption += `⭔ extension : ${i.medias[0].extension}\n`
+        caption += `⭔ size : ${i.medias[0].size}\n`
+        caption += `⭔ formattedSize : ${i.medias[0].formattedSize}\n`
+        killua.sendFile(m.from, fetch.result.medias[0].url, "", m, { caption })
     },
     isQuery: true
 }

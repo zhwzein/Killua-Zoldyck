837457e750b7a8976dbde37230c08a57a1406c57
zahwazein <zhwzein.me@gmail.com>
2022-05-30 14:16:42 +0700

    Update myip.js
    Cara alternatif buat yg malas update IP lewat Website, now you can change it without login to website by doing this command, for example:
    
    $setip <your_new_ip>

--- commands/owner/myip.js
+++ commands/owner/myip.js
@@ -1,17 +1,23 @@
-var http = require('http')
+const http = require('http')
+const { fetchUrl } = require("../../lib/Function")
 
 module.exports = {
     name: "myip",
-    alias: ["getip"],
-    desc: "Get My IP Public Information",
+    alias: ["getip", "setip"],
+    desc: "Get My IP Public Information & Set My Ip",
     type: "owner",
     example: "%prefix%command",
-    start: async(killua, m) => {
-        http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
-            resp.on('data', function(ip) {
-                m.reply("My public IP address is: " + ip);
+    start: async(killua, m, { text }) => {
+        if (text) {
+            let fetch = await fetchUrl(global.api("zenz", "/user/customip", { query: text }, "apikey"))
+            m.reply("This will change your default IP on the web\n\n" + fetch.result)
+        } else {
+            http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
+                resp.on('data', function(ip) {
+                    m.reply("My public IP address is: " + ip);
+                })
             })
-        })
+        }
     },
     isOwner: true
 }

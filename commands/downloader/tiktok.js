61f9425bdc9d6a5eb6909fbcf46d25ddf634b069
zahwazein <zhwzein.me@gmail.com>
2022-07-12 17:36:41 +0700

    Update tiktok.js

--- commands/downloader/tiktok.js
+++ commands/downloader/tiktok.js
@@ -6,21 +6,42 @@ module.exports = {
     use: "<url>",
     desc: "Download Media From https://tiktok.com",
     type: "downloader",
-    example: "%prefix%command <url>",
+    example: "%prefix%command <username> or <url>",
     start: async(killua, m, { text }) => {
-        let fetch = await fetchUrl(global.api("zenz", "/downloader/musically", { url: isUrl(text)[0] }, "apikey"))
-        let buttons = [
-            {buttonId: `tiktokwm ${text}`, buttonText: {displayText: '► With Watermark'}, type: 1},
-            {buttonId: `tiktokmp3 ${text}`, buttonText: {displayText: '♫ Audio'}, type: 1}
-        ]
-        let buttonMessage = {
-            video: { url: fetch.result.nowm },
-            caption: `Download Tiktok From : ${isUrl(text)[0]}`,
-            footer: config.footer,
-            buttons: buttons,
-            headerType: 5
+        if (isUrl(text)) {
+            let fetch = await fetchUrl(global.api("zenz", "/downloader/musically", { url: isUrl(text)[0] }, "apikey"))
+            let buttons = [
+                {buttonId: `tiktokwm ${text}`, buttonText: {displayText: '► With Watermark'}, type: 1},
+                {buttonId: `tiktokmp3 ${text}`, buttonText: {displayText: '♫ Audio'}, type: 1}
+            ]
+            let buttonMessage = {
+                video: { url: fetch.result.nowm },
+                caption: `Download Tiktok From : ${isUrl(text)[0]}`,
+                footer: config.footer,
+                buttons: buttons,
+                headerType: 5
+            }
+            killua.sendMessage(m.from, buttonMessage, { quoted: m })
+        } else {
+            let fetch = await fetchUrl(global.api("zenz", "/downloader/asupantiktok", { query: text }, "apikey"))
+            let caption = `Random Asupan Tiktok ${text}\n\n`
+            let i = fetch.result
+            caption += `⭔ Username : ${i.username}\n`
+            caption += `⭔ Followers : ${i.followers}\n`
+            caption += `⭔ Caption : ${i.media.caption}\n`
+
+            let buttons = [
+                {buttonId: `tiktok ${text}`, buttonText: {displayText: '► NEXT'}, type: 1},
+            ]
+            let buttonMessage = {
+                video: { url: i.media.videourl },
+                caption: caption,
+                footer: config.footer,
+                buttons: buttons,
+                headerType: 5
+            }
+            killua.sendMessage(m.from, buttonMessage, { quoted: m })
         }
-        killua.sendMessage(m.from, buttonMessage, { quoted: m })
     },
     isQuery: true
 }

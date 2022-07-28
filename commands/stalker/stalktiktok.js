916f2581723e220d84c3499603983814d4abc215
zahwazein <zhwzein.me@gmail.com>
2022-07-28 15:02:41 +0700

    Fix STALKIG & Add STALK TIKTOK

--- commands/stalker/stalktiktok.js
+++ commands/stalker/stalktiktok.js
@@ -1,22 +1,21 @@
 const { fetchUrl } = require("../../lib/Function")
 
 module.exports = {
-    name: "stalkig",
-    alias: ["instagramstalk"],
+    name: "stalktiktok",
+    alias: ["tiktokstalk"],
     use: "<username>",
-    desc: "Instagram Profile Stalker",
+    desc: "TikTok Profile Stalker",
     type: "stalker",
     example: `%prefix%command <username>`,
     start: async(killua, m, { text }) => {
-        let fetch = await fetchUrl(global.api("zenz", "/stalker/ig", { username: text }, "apikey"))
-        let caption = `Instagram Profile Stalker :\n\n`
-        let i = fetch.result.caption
-        caption += `⭔ Fullname : ${i.fullname}\n`
+        let fetch = await fetchUrl(global.api("zenz", "/stalker/tiktok", { username: text }, "apikey"))
+        let caption = `TikTok Profile Stalker :\n\n`
+        let i = fetch.result
+        caption += `⭔ Name : ${i.name}\n`
         caption += `⭔ Username : ${i.username}\n`
-        caption += `⭔ Post : ${i.post}\n`
         caption += `⭔ Followers : ${i.followers}\n`
         caption += `⭔ Following : ${i.following}\n`
-        caption += `⭔ Bio : ${i.bio}\n`
+        caption += `⭔ Description : ${i.description}\n`
         killua.sendFile(m.from, i.profile, "", m, { caption })
     },
     isQuery: true

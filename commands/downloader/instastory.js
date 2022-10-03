73d33b9081961345f8a63db1de15d4aad2bf4452
Fatih Arridho <71856084+FatihArridho@users.noreply.github.com>
2022-10-03 08:38:34 +0700

    fix

--- commands/downloader/instastory.js
+++ commands/downloader/instastory.js
@@ -13,7 +13,7 @@ module.exports = {
             killua.sendFile(m.from, fetch.result[0], "", m, { caption: `Download Story From : ${isUrl(text)[0]}` })
         } else {
             let fetch = await fetchUrl(global.api("zenz", "/downloader/instagram/story", { username: text }, "apikey"))
-            for (let i of fetch.result) killua.sendFile(m.from, i.url_list, "", m, { caption: `Download Story From : ${text}` })
+            for (let i of fetch.result.url_list) killua.sendFile(m.from, i, "", m, { caption: `Download Story From : ${text}` })
         }
     },
     isQuery: true
